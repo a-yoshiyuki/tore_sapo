a3e3e354ec4cb1e071c40004d540db059db838ba
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-01-05 06:32:01 +0900

    [Add]Vue.js 余分なコード削除

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,41 +1,18 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
-    <dl>
-      <dt @click="isOpen" class="slideHead">
-        <p class="headText">詳しくはこちら</p>
-      </dt>
-      <SlideUpDown :active="isActive">
-        <dd class="slideBody">
-          <p>This is vue-slide-up-down!</p>
-        </dd>
-      </SlideUpDown>
-      <dd class="slideFoot">
-        <p>★★★★</p>
-      </dd>
-    </dl>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import SlideUpDown from 'vue-slide-up-down'
 
-export default Vue.extend({
+export default ({
   data: function () {
     return {
       message: "Hello Vue!",
-      isActive: false,
     }
   },
-  components: {
-    SlideUpDown,
-  },
-  methods: {
-    isOpen() {
-      this.isActive = !this.isActive
-    },
-  }
 })
 </script>
 
@@ -43,36 +20,5 @@ export default Vue.extend({
 p {
   font-size: 2em;
   text-align: center;
-}
-
-.slideHead {
-  height: 50px;
-  width: 30%;
-  margin-top: 30px;
-  margin-left: 30px;
-  background-color: #ea0069;
-}
-.headText {
-  color: #0e0070;
-  font-size: 20px;
-  text-align: center;
-}
-.slideBody {
-  height: 100px;
-  width: 30%;
-  margin-left: 30px;
-  color: #FFFFFF;
-  font-size: 20px;
-  text-align: center;
-  background-color: #0e0070;
-}
-.slideFoot {
-  height: 30px;
-  width: 30%;
-  margin-left: 30px;
-  color: #FFFFFF;
-  font-size: 15px;
-  text-align: center;
-  background-color: #FFBBFF;
 }
 </style>
