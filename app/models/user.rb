96a5d8d539e4336d062fde2d73e5728330e1183b
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-01-10 09:00:38 +0900

    [Add]Relationshipテーブルへのフォロー保存　成功

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :relationships, dependent: :destroy
-  has_many :followings, through: :relationships, source: :follow
+  has_many :followings, through: :relationships, source: :follower
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :user
 
