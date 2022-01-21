a6b3305c7ddfdb8fea54c366c0ee78bfed671546
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-01-21 18:46:34 +0900

    [Add]_side_bar.html.erb 変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import Rails from "@rails/ujs"
+import SlideUpDown from 'vue-slide-up-down'
 
 require('jquery')
 require('../side_bar.js')
 require('../main.js')
 
 Rails.start()
+SlideUpDown.start()
