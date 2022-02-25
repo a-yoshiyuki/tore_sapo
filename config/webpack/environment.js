b4d691fa6bf37036b509058451c31b836ca8579c
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-02-26 08:34:31 +0900

    [Add]config/webpack/enviroment.js vue-loaderの記述削除

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
@@ -9,8 +7,6 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
     Popper: ['popper.js', 'default']
 }))
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 module.exports = environment
 
 environment.config.merge({
