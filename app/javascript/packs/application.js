beca1134e693f3b6e81f37bed6c5db57c20ea201
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-02-26 08:29:44 +0900

    [Add]モーダルウィンドウ　表示成功

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import Turbolinks from 'turbolinks'
 require('jquery')
 require('./main.js')
 require('./side_bar.js')
+require('bootstrap/dist/js/bootstrap.min.js')
 
 Rails.start()
 Turbolinks.start()
