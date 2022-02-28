fc5e294974b7726a5ba5da38108352fde9d6d0d2
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-02-28 22:26:43 +0900

    [Add]アソシエーション再度修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
-  belongs_to :participant_user
   belongs_to :message_room
 end
--- app/models/message_room.rb
+++ app/models/message_room.rb
@@ -1,6 +1,4 @@
 class MessageRoom < ApplicationRecord
   has_many :user_message_rooms
   has_many :messages
-  has_many :users, through: :user_message_rooms
-  has_many :participant_users, through: :user_message_rooms
 end
