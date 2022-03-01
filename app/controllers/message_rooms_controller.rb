37d38eec61ab6e1011daf3877c389245dc455744
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-03-01 14:03:26 +0900

    [Add]message_rooms_controller.rb 整理

--- app/controllers/message_rooms_controller.rb
+++ app/controllers/message_rooms_controller.rb
@@ -6,21 +6,9 @@ class MessageRoomsController < ApplicationController
   end
 
   def create
-    current_user_message_rooms = UserMessageRoom.where(user_id: current_user.id).map(&:message_room)
-    message_room = UserMessageRoom.where(message_room: current_user_message_rooms, user_id: params[:user_id]).map(&:message_room).first
-    
-    if message_room.blank?
-      message_room = MessageRoom.create
-      UserMessageRoom.create(message_room: message_room, user_id: current_user.id)
-      UserMessageRoom.create(message_room: message_room, user_id: params[:user_id])
-    end
-
-    redirect_to action: :show, id: message_room.id
   end
 
   def show
     @message_room = MessageRoom.find(params[:id])
-    @user_message_room = @message_room.user_message_rooms.where.not(user_id: current_user.id).first.user
-    @messages = Message.where(message_room: @message_room)
   end
 end
