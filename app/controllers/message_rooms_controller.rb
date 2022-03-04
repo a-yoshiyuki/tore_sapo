d51d8934f03dd40c2d284ec0082ae750ae5e755f
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-03-04 09:15:47 +0900

    [Add]ページ遷移　変更

--- app/controllers/message_rooms_controller.rb
+++ app/controllers/message_rooms_controller.rb
@@ -10,14 +10,15 @@ class MessageRoomsController < ApplicationController
     message_room = UserMessageRoom.find_by(user_id: current_user.id, participant_user_id: participant_user.id)&.message_room
 
     if message_room
-      redirect_to message_room_path(message_room_id: @message_room.id)
+      redirect_to message_room_path(id: message_room.id)
     else
       message_room = MessageRoom.create
-      redirect_to message_room_path(message_room_id: message_room.id)
+      redirect_to message_room_path(id: message_room.id)
     end
   end
 
   def show
-    @message_room = MessageRoom.find(params[:id])
+    #@message_room = MessageRoom.find(params[:id])
+    @participant_user = User.find_by(params[:participant_user_id])
   end
 end
