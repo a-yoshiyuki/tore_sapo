821f068aaed1e3b6cea82e87d985d6a19c8fde7b
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-04-04 22:04:45 +0900

    [Add]user_message_room.rb ユニーク制約追加 => 未解決

--- app/models/user_message_room.rb
+++ app/models/user_message_room.rb
@@ -2,4 +2,6 @@ class UserMessageRoom < ApplicationRecord
   belongs_to :message_room
   belongs_to :user
   belongs_to :participant_user, class_name: 'User'
+
+  validates :message_room_id, uniqueness: { scope: :user_id }
 end
