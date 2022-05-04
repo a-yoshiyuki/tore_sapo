b1ca16085a9911628703d470ca94c5b551b0ec1a
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-05-04 22:23:06 +0900

    [Fix]model 変更

--- app/models/message_room.rb
+++ app/models/message_room.rb
@@ -2,5 +2,4 @@ class MessageRoom < ApplicationRecord
   has_many :user_message_rooms
   has_many :messages
   has_many :users, through: :user_message_rooms
-  has_many :participant_users, through: :user_message_rooms
 end
