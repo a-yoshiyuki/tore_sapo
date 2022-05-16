dadfd419144c7768701496cb110b72dc0f22a4f5
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2022-05-16 09:34:26 +0900

    [Fix]messages_controller 変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    if UserMessageRoom.where(user_id: current_user.id, message_room_id: message_params[:message_room_id], participant_user_id: message_params[:participant_user_id]).present?
+    if UserMessageRoom.where(user_id: current_user.id, message_room_id: params[:message][:message_room_id]).present?
       @message = Message.create!(message_params.slice(:user_id, :message_room_id, :content))
       redirect_to message_room_path(@message.message_room_id)
     else
@@ -12,6 +12,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:user_id, :message_room_id, :participant_user_id, :content).merge(user_id: current_user.id)
+    params.require(:message).permit(:user_id, :message_room_id, :content).merge(user_id: current_user.id)
   end
 end
